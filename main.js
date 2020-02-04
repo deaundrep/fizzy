075bd78376500674c4ff79b665e40b98a8f15b6e
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-02-04 14:54:08 -0500

    done

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function fizzy(num) {
-
+function fizzy(num){
+  if(num % 3 === 0 && num % 5 === 0){
+    return "FizzBuzz"
+  }
+  if (num % 3 === 0){
+    return "Fizz"
+  }
+  if (num % 5 === 0){
+    return "Buzz" 
+  }
+  return (num)
 }
 
 module.exports = {
